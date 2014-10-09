aa807ddcdf2248c97209d5d4e4abb7e4841e6b17
Ethan Bennis <etb513@gmail.com>
2014-10-09 17:58:36 -0400

    Updated net_include.h

--- net_include.h
+++ net_include.h
@@ -10,19 +10,40 @@
 #include <netdb.h>
 #include <errno.h>
 
-#define PORT	        10010
-/* somehow define mcast IP: 255.1.2.101 */
-#define MAX_PACKET_SIZE 1400
-#define PACKET_ID int
-#define PACKET_TYPE int
-#define PAYLOAD_SIZE    MAX_PACKET_SIZE-sizeof(PACKET_TYPE)-sizeof(PACKET_ID)
-#define BURST_MAX       20
-#define MEG             1048576
-#define MEG50           50*MEG
+#define MCAST_PORT	    10010
+#define MCAST_IP        4278256229  // IP: 255.01.02.101
+#define BURST_MSG       20
+#define WINDOW_SIZE     2048
+#define MAX_PACKET_SIZE WINDOW_SIZE * 4 + 12
+#define PAYLOAD_SIZE    1200
+#define BURST_TOKEN     2
+#define PACKET_ID       int
+#define PACKET_TYPE     int
 
 /* Packet: Struct for generic packet */
 typedef struct {
     PACKET_TYPE type;
     char payload[MAX_PACKET_SIZE- sizeof(PACKET_TYPE)];    
 } Packet;
+
+/* McastToken: Struct for multicasted token */
+typedef struct {
+    PACKET_TYPE type;
+    int         seq;
+    int         aru;
+    int         recv;
+    int         ip_array[10];   // Max 10 machines
+    int         rtr[MAX_PACKET_SIZE - 52];
+} McastToken;
+
+/* Token: Struct for standard unicasted token */
+typedef struct {
+    PACKET_TYPE type;
+    int         seq;
+    int         aru;
+    int         sndr;
+    int         recv;
+    int         ip_array[10];   // Max 10 machines
+    int         rtr[MAX_PACKET_SIZE - 56];
+} Token;
 
